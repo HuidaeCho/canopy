d309bdc889d8f522ac967a307027620259a2a813
ocsmit <owen8461@protonmail.com>
2020-02-01 21:33:16 -0500

    Fix header

--- canopy.py
+++ canopy.py
@@ -1,10 +1,9 @@
 ################################################################################
-# Name:    canopy.py
-# Purpose: This module provides utility functions for preprocessing NAIP tiles
-#          and postprocessing trained canopy tiles.
-# Authors: Huidae Cho, Ph.D., Owen Smith, IESA, University of North Georgia
-# Since:   November 29, 2019
-# Grant:   Sponsored by the Georgia Forestry Commission through the Georgia
+# Name:    canopy.py # Purpose: This module provides utility functions for 
+#          preprocessing NAIP tiles and postprocessing trained canopy tiles. 
+# Authors: Huidae Cho, Ph.D., Owen Smith, IESA, University of North Georgia 
+# Since:   November 29, 2019 
+# Grant:   Sponsored by the Georgia Forestry Commission through the Georgia 
 #          Statewide Canopy Assessment Phase I: Canopy Analysis 2009 project
 ################################################################################
 
